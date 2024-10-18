1f9d2540a18f2c21c687208e9112d3ab028840b6
Shalini Chaturvedy <shalinichaturvedy@gmail.com>
2024-10-18 10:53:03 +0530

    Adding the new button

---
+++ PrimeShop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrimeshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PrimeShop'
---
+++ PrimeShop/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from PrimeShop import views
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('contact',views.contact,name="contact"),
+    path('about',views.about,name="about"),
+]
