be11e58aebb1714d2525739050b1c84b27203ca0
Shalini Chaturvedy <shalinichaturvedy@gmail.com>
2024-11-04 10:14:23 +0530

    Adding Portfolio and remore signUP

--- authcart/views.py
+++ authcart/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
-    return render(request,"signup.html")
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+
+        if password != confirm_password:
+            return HttpResponse("Password incorrect.")
+
+        # Check if the email already exists
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse("Email already exists.")
+        except User.DoesNotExist:
+            # This will only execute if the email doesn't already exist
+            user = User.objects.create_user(username=email, email=email, password=password)
+            user.is_active = False
+            user.save()
+            return HttpResponse("User created")
+
+    # If GET request or after form submission
+    return render(request, "signup.html")
+
 
 def handlelogin(request):
-    return render(request,"login.html")
+    return render(request, "login.html")
+
 
 def handlelogout(request):
     return redirect('/auth/login')
-
